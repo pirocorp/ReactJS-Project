4e83b9ed91a3b4a7a1cc51c0c8ee9d8fda82afe5
Zdravko Zdravkov <pirocorp@abv.bg>
2021-03-24 14:30:40 +0100

    React client initial

---
+++ src/client/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Header from './components/Header';
+
+function App() {
+    return (
+        <div className="main-wrapper">
+            <Header></Header>
+            <h1>Hello World!</h1>
+        </div>
+    );
+}
+
+export default App;
---
+++ src/client/src/components/Header/Header.js
@@ -0,0 +1,13 @@
+import './Header.css';
+
+import
+
+const Header = () => {
+    return(
+        <header class="header">
+            <Navbar></Navbar>
+        </header>
+    );
+}
+
+export default
